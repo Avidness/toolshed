bdc23c9318d7a511b9ae60a305a8ca934eddfd41
Avidness <adavidness@gmail.com>
2018-04-28 11:43:05 -0500

    Set the primary key

--- Models/Entity.cs
+++ Models/Entity.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToolShed.Models.Domain
 {
     public class Entity
     {
+        [Key]
         public virtual int Id { get; set; }
 
         [DisplayName("Created At")]
