d08fa450fb678bf4aee72198879e23f6eadf1808
Avidness <adavidness@gmail.com>
2018-05-06 02:39:40 -0500

    Add menu bar

--- ClientApp/app.js
+++ ClientApp/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemManagement from './components/Items/ItemManagement';
+import MenuAppBar from './components/MenuAppBar'
+import 'normalize.css'
 
 ReactDOM.render(
-    <ItemManagement />,
+    <div>
+        <MenuAppBar />
+        <ItemManagement />
+    </div>,
     document.getElementById('app')
 );
