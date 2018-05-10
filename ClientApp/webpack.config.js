fa19a0a1c00057632ddbf0a383a777077fed76fd
Avidness <adavidness@gmail.com>
2018-05-09 22:19:00 -0500

    Update npm packages and webpack config

--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -1,46 +1,44 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = (env) => {
-    const isDevBuild = !(env && env.prod);
-    return [{
-        context: __dirname,
-        entry: "./app.js",
-        output: {
-            path: path.resolve(__dirname, '../', 'wwwroot/dist'),
-            filename: 'bundle.js',
-            publicPath: '/'
+module.exports = {
+  context: __dirname,
+  entry: "./app.js",
+  output: {
+      path: path.resolve(__dirname, '../', 'wwwroot/dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+  },
+  resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react']
+          }
         },
-        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-        watch: true,
-        module: {
-          rules: [
-            {
-              test: /\.jsx?$/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['babel-preset-env', 'babel-preset-react']
-                }
-              },
-              exclude: /node_modules/,
-            },
-            {
-              test: /\.css$/,
-              use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{
-                    loader: 'css-loader',
-                    options: {
-                      modules: true,
-                      importLoaders: 1,
-                      sourceMap: true,
-                      localIdentName: '[path]--[name]-[local]--[hash:base64:5]',
-                    },
-                  }],
-              }),
-            },
-          ]
-        }
-    }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      },
+    ]
+  }
 };
