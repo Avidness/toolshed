51d42e408d27459a9f65ff09cb21d1d81e9523ed
Avidness <adavidness@gmail.com>
2018-05-13 16:29:41 -0500

    Add normalize.css and svg loader

--- ClientApp/app.js
+++ ClientApp/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemManagement from './components/Items/ItemManagement';
 import MenuAppBar from './components/MenuAppBar'
-import 'normalize.css'
+import 'normalize.css/normalize.css';
 
 ReactDOM.render(
     <div>
